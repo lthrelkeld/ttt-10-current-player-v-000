8eae9776f04890b9cf7fe1d76b1a7dc59284ca6f
Elizabeth Karst <lkarst@outlook.com>
2018-08-11 19:49:17 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,7 @@
 def turn_count(board)
   count = 0
   board.each do |spot_on_board|
-    if spot_on_board == "X" || spot_on_board == "O"
-      count += 1
-    end
+    count += 1 if spot_on_board == "X" || spot_on_board == "O"
   end
   count
 end
